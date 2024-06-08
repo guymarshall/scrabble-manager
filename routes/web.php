5e449bb1e4192608528344fe591b5ebaa5c10bbf
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-06-08 15:06:35 +0100

    UPDATE: Add web routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', static function () {
+    return view('home');
+})->name('home');
+
+Route::get('/profiles', static function () {
+    return view('profiles');
+})->name('profiles');
+
+Route::get('/profile/{userId}', static function (int $userId) {
+    return view('profile', ['userId' => $userId]); // TODO: check userId exists in controller
+})->whereNumber('userId')->name('profile');
+
+Route::get('/games', static function () {
+    return view('games');
+})->name('games');
+
+Route::get('/game/{gameId}', static function (int $gameId) {
+    return view('game', ['gameId' => $gameId]); // TODO: check gameId exists in controller
+})->whereNumber('gameId')->name('game');
+
+Route::get('leaderboard', static function () {
+    return view('leaderboard');
+})->name('leaderboard');
