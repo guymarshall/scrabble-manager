825b204344eb68f028ade6a47277dd8ad23dfb58
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-06-08 16:42:52 +0100

    UPDATE: Add migration to create games table

---
+++ database/migrations/2024_06_08_150747_create_games_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', static function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+
+        DB::statement('ALTER TABLE games ADD COLUMN player_ids integer[]');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
