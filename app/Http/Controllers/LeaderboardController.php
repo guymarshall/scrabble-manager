c950eb93ef1661d78be6001ca28aa9d5c8a44dc8
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2024-06-09 12:06:12 +0100

    FIX: Leaderboard now sorts by average score not single game score

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class LeaderboardController extends Controller
@@ -13,10 +14,10 @@ class LeaderboardController extends Controller
     public function show(): View
     {
         $usersWithScores = User::query()
-            ->select(['users.name AS name', 'users.id AS user_id', 'scores.score'])
-            ->leftJoin('scores', 'scores.user_id', '=', 'users.id')
-            ->where('scores.score', '>', 0)
-            ->orderBy('score', 'desc')
+            ->select('users.id', 'users.name', DB::raw('AVG(scores.score) as average_score'))
+            ->join('scores', 'users.id', '=', 'scores.user_id')
+            ->groupBy('users.id', 'users.name')
+            ->orderBy('average_score', 'desc')
             ->limit(10)
             ->get();
 
--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -3,7 +3,7 @@
 <h1>Leaderboard</h1>
 
 @foreach($usersWithScores as $user)
-    <a href="{{ URL::route('profile', $user->user_id) }}"><h3>{{ $user->name . ' - ' . $user->score }}</h3></a>
+    <a href="{{ URL::route('profile', $user->id) }}"><h3>{{ $user->name . ' - ' . $user->average_score }}</h3></a>
 @endforeach
 
 @include('templates.end')
